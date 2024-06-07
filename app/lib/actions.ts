5006f60150e93cca46192d909e88ee9b4d76f224
JM Acera <john.acera@sportality.se>
2024-06-07 17:13:05 +0800

    add sign up functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,6 +7,7 @@ import { z } from 'zod';
 import { auth, getUserdata, signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 import { getUser } from './data';
+import bcrypt from 'bcrypt';
 
 const HelpAppFormSchema = z.object({
   id: z.string(),
@@ -144,4 +145,26 @@ export async function authenticate(
     }
     throw error;
   }
+}
+
+export async function createUser(name: string, email:string, password: string, department: string) {
+  const userdata: any = await getUserdata();
+  if (userdata?.email === email) {
+    return {
+      errors: 'Error',
+      message: 'Email already created. Failed to Create User.',
+    };
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await sql`
+      INSERT INTO users (name, email, password, department)
+      VALUES (${name}, ${email}, ${hashedPassword}, ${department})
+    `;
+
+  } catch(error) {
+    return { message: 'Database Error: Failed to Create User.' };
+  }
+  revalidatePath('/login');
+  redirect('/login');
 }
