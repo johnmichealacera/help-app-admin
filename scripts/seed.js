d900d1666efe062f6cc6277f0fdcaac170994519
JM Acera <john.acera@sportality.se>
2024-06-07 17:16:13 +0800

    modify seed script

--- scripts/seed.js
+++ scripts/seed.js
@@ -15,7 +15,7 @@ async function seedUsers(client) {
         name VARCHAR(255) NOT NULL,
         email TEXT NOT NULL UNIQUE,
         department TEXT NOT NULL,
-        image_url TEXT NOT NULL,
+        image_url TEXT,
         password TEXT NOT NULL
       );
     `;
@@ -42,41 +42,6 @@ async function seedUsers(client) {
     };
   } catch (error) {
     console.error('Error seeding users:', error);
-    throw error;
-  }
-}
-
-async function seedRevenue(client) {
-  try {
-    // Create the "revenue" table if it doesn't exist
-    const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS revenue (
-        month VARCHAR(4) NOT NULL UNIQUE,
-        revenue INT NOT NULL
-      );
-    `;
-
-    console.log(`Created "revenue" table`);
-
-    // Insert data into the "revenue" table
-    const insertedRevenue = await Promise.all(
-      revenue.map(
-        (rev) => client.sql`
-        INSERT INTO revenue (month, revenue)
-        VALUES (${rev.month}, ${rev.revenue})
-        ON CONFLICT (month) DO NOTHING;
-      `,
-      ),
-    );
-
-    console.log(`Seeded ${insertedRevenue.length} revenue`);
-
-    return {
-      createTable,
-      revenue: insertedRevenue,
-    };
-  } catch (error) {
-    console.error('Error seeding revenue:', error);
     throw error;
   }
 }
@@ -140,9 +105,8 @@ async function main() {
   const client = await db.connect();
 
   await seedUsers(client);
-  await seedRevenue(client);
-  await seedAnnouncements(client);
-  await seedReports(client);
+  // await seedAnnouncements(client);
+  // await seedReports(client);
 
   await client.end();
 }
